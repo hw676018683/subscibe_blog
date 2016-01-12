09141fc5c379e634297c4b69f174ca94d137d606
hw93 <676018683@qq.com>
2016-01-12 11:21:44 +0800

    create table user_blogs

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160112023008) do
+ActiveRecord::Schema.define(version: 20160112023638) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,14 @@ ActiveRecord::Schema.define(version: 20160112023008) do
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
     t.string   "articles",   default: [],              array: true
+  end
+
+  create_table "user_blogs", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "blog_id"
+    t.string   "read_articles", default: [],              array: true
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
   end
 
   create_table "users", force: :cascade do |t|
