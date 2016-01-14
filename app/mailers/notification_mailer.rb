e5f3ea60f7d08754cfacb5669aed26d8d5312802
hw93 <676018683@qq.com>
2016-01-14 12:22:57 +0800

    read blog after send email

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,6 +2,7 @@ class NotificationMailer < ApplicationMailer
 
   def notice_update subscription
     @subscription = subscription
-    mail to: @subscription.user.email, subject: '博客跟新通知'
+    mail to: @subscription.user.email, subject: '博客更新通知'
+    subscription.read_blog!
   end
 end
