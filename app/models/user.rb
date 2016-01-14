dd108f78711750833618c896f66ac03f65d7cd6c
hw93 <676018683@qq.com>
2016-01-14 16:45:03 +0800

    update user

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,4 +14,13 @@ class User < ActiveRecord::Base
   def subscibe? blog
     subscriptions.exists?(blog: blog)
   end
+
+  def update_with_password(params = {})
+    if !params[:current_password].blank? || !params[:password].blank? || !params[:password_confirmation].blank?
+      super
+    else
+      params.delete(:current_password)
+      update_without_password(params)
+    end
+  end
 end
