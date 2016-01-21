ce9f973680e412bd8556caef08587717f0a23918
hw93 <676018683@qq.com>
2016-01-21 10:54:39 +0800

    set deploy  user

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :repo_url, 'git@github.com:hw676018683/subscibe_blog.git'
 
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
-
+set :user,            'deploy'
 # Don't change these unless you know what you're doing
 set :pty,             true
 set :use_sudo,        false
