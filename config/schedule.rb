aecceffbe6fae6f2d2b8f97ecd51b58c2764720d
hw93 <676018683@qq.com>
2016-01-26 19:46:46 +0800

    set log

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,3 +1,3 @@
 every 1.day, at: '1:30 am' do
-  rake 'blog:get_articles', output: { error: Settings.whenever.log.error, :standard => Settings.whenever.log.crons }
+  rake 'blog:get_articles', output: { error: ENV['WHENEVER_ERROR_LOG'], standard: ENV['WHENEVER_CRONS_LOG'] }
 end
